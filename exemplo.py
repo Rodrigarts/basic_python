b371382f8a0617b8091cdb8fe780e67160f3a3b7
Rodrigarts <rodrigofonsecaguedes@gmail.com>
2018-10-11 16:23:50 +0200

    Create exemplo.py

---
+++ exemplo.py
@@ -0,0 +1,25 @@
+def calc_1(x,y,z,w):
+    print("hue1")
+
+def calc_2(x):
+    print("hue2")
+	
+shapes_enum = {lua,
+               bola}
+			   
+dicionario = {
+        shapes_enum.lua:calc_1,
+        shapes_enum.bola:calc_2
+    }
+
+if __name__ == '__main__':
+
+	#nao importa
+	
+	
+    
+	
+	
+def _do_calc(shapes_enum)
+
+	dicionario[shapes_enum](?????)
